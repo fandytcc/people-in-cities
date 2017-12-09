6208c9e662066cc5c5132f240b8c324ee82adeba
iskanderbroere <iskanderbroere@gmail.com>
2017-12-09 13:55:36 +0100

    Update cities_controller.rb
    fix else typo

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -15,7 +15,8 @@ class CitiesController < ApplicationController
  def create
    @city = City.new(city_params)
    if @city.save
-     redirect_to @city_pathelse
+     redirect_to @city_path
+   else
      render 'new'
    end
  end
