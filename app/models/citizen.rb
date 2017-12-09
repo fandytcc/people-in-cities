b369641f1e4aa83f1785968fb6527d8519396a90
Iskander Broere <iskanderbroere@gmail.com>
2017-12-09 15:31:42 +0100

    validations in citizen model, pretty lines in the index and error messages in the form partial

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -1,3 +1,8 @@
 class Citizen < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :age, length: { maximum: 2 }, allow_blank: true
+  validates :occupation, length: { maximum: 255 }, allow_blank: true
   belongs_to :city
 end
