c31ecb8d1e4ffbfc0cbbddb6d2588c2db5c7544c
Iskander Broere <iskanderbroere@gmail.com>
2017-12-09 13:04:04 +0100

    added index and generated a controller

---
+++ app/controllers/citizens_controller.rb
@@ -0,0 +1,42 @@
+class CitizensController < ApplicationController
+  def index
+    @citizens = Citizen.all
+  end
+  def show
+    @citizen = Citizen.find(params[:id])
+  end
+  def new
+    @citizen = Citizen.new
+  end
+  def create
+    @citizen = Citizen.new(citizen_params)
+    if @citizen.save
+      redirect_to @citizen
+    else
+      render 'new'
+    end
+  end
+  def edit
+    @citizen = Citizen.find(params[:id])
+  end
+  def update
+    @citizen = Citizen.find(params[:id])
+
+    if @citizen.update_attributes(citizen_params)
+      redirect_to @citizen
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @citizen = Citizen.find(params[:id])
+    @citizen.destroy
+    redirect_to citizens_path
+  end
+
+  private
+
+  def citizen_params
+    params.require(:citizen).permit(:name, :age, :image_url, :valley_id)
+  end
+end
